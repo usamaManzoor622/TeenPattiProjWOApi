93e26db2eedffe70d5874229cf5e9b73e4cf1541
AmjadTechSphere <amjadtechsphere@gmail.com>
2026-04-30 23:10:03 +0500

    update firebase sd and remove facebook sdk

--- Assets/-MyGame/MyScripts/Authentication/Facebookauth.cs
+++ Assets/-MyGame/MyScripts/Authentication/Facebookauth.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Facebook.Unity;
 using Firebase.Auth;
 using System;
 using UnityEngine.UI;
@@ -44,22 +43,15 @@ public class Facebookauth : MonoBehaviour
     {
         if (_instance == null)
             _instance = this;
-        if (!FB.IsInitialized)
-        {
-            FB.Init(InitCallBack, OnHideUnity);
-        }
-        else
-        {
-            FB.ActivateApp();
-        }
+       
     }
     private void InitCallBack()
     {
-        if (!FB.IsInitialized)
-        {
-            FB.ActivateApp();
-        }
-        else
+      //  if (!FB.IsInitialized)
+        //{
+        //    FB.ActivateApp();
+        //}
+        //else
         {
             IdText.text = "Failed to initialize";
             //Debug.Log("Failed to initialize");
@@ -81,12 +73,12 @@ public class Facebookauth : MonoBehaviour
     {
         LoginWithAllAuth.Instance.WaitPanel.SetActive(true);
         var permission = new List<string>() { "public_profile", "email" };
-        FB.LogInWithReadPermissions(permission, AuthCallBack);
+      //  FB.LogInWithReadPermissions(permission, AuthCallBack);
     }
 
     public void FaceBook_LogOut()
     {
-        FB.LogOut();
+       // FB.LogOut();
     }
 
     private void RetrieveFriendsList()
@@ -94,28 +86,28 @@ public class Facebookauth : MonoBehaviour
         Debug.Log("Friends List Calling");
         // Call the FriendsCallBack method to retrieve the friends listvar permission = new List<string>() { "public_profile", "email" };
         var permission = new List<string>() { "user_friends", "email" };
-        FB.API("/me/friends", HttpMethod.GET, FriendsCallBack);
+        //FB.API("/me/friends", HttpMethod.GET, FriendsCallBack);
     }
 
-    private void FriendsCallBack(IGraphResult result)
-    {
-        if (result.Error == null)
-        {
-            var friendsData = result.ResultDictionary["data"] as List<object>;
-            Debug.Log(friendsData.Count);
-            foreach (var friend in friendsData)
-            {
-                var friendData = friend as Dictionary<string, object>;
-                var friendId = friendData["id"].ToString();
-                var friendName = friendData["name"].ToString();
-                Debug.Log("Friend ID: " + friendId + ", Name: " + friendName);
-            }
-        }
-        else
-        {
-            Debug.Log("Failed to retrieve friends list: " + result.Error);
-        }
-    }
+    //private void FriendsCallBack(IGraphResult result)
+    //{
+    //    if (result.Error == null)
+    //    {
+    //        var friendsData = result.ResultDictionary["data"] as List<object>;
+    //        Debug.Log(friendsData.Count);
+    //        foreach (var friend in friendsData)
+    //        {
+    //            var friendData = friend as Dictionary<string, object>;
+    //            var friendId = friendData["id"].ToString();
+    //            var friendName = friendData["name"].ToString();
+    //            Debug.Log("Friend ID: " + friendId + ", Name: " + friendName);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Failed to retrieve friends list: " + result.Error);
+    //    }
+    //}
 
     private void RetrieveProfilePicture()
     {
@@ -125,65 +117,65 @@ public class Facebookauth : MonoBehaviour
         { "redirect", "false" }
     };
 
-        FB.API("me/picture", HttpMethod.GET, ProfilePictureCallBack1, parameters);
+        //FB.API("me/picture", HttpMethod.GET, ProfilePictureCallBack1, parameters);
     }
 
-    private void AuthCallBack(ILoginResult result)
-    {
-        if (FB.IsLoggedIn)
-        {
-            var aToken = AccessToken.CurrentAccessToken;
-            Debug.Log(AccessToken.CurrentAccessToken.ExpirationTime);
-            RetrieveProfilePicture();
-            IdText.text = (aToken.UserId);
-            //   LoginWithAllAuth.Instance.GetTokenIDAndOtherThing(3, aToken.UserId, "", "", null);
-            tokenId = aToken.UserId;
-            IdText.gameObject.SetActive(true);
-            Debug.Log(aToken.UserId);
+//    private void AuthCallBack(ILoginResult result)
+//    {
+//        if (FB.IsLoggedIn)
+//        {
+//            var aToken = AccessToken.CurrentAccessToken;
+//            Debug.Log(AccessToken.CurrentAccessToken.ExpirationTime);
+//            RetrieveProfilePicture();
+//            IdText.text = (aToken.UserId);
+//            //   LoginWithAllAuth.Instance.GetTokenIDAndOtherThing(3, aToken.UserId, "", "", null);
+//            tokenId = aToken.UserId;
+//            IdText.gameObject.SetActive(true);
+//            Debug.Log(aToken.UserId);
 
-            string accesstoken;
-            string[] data;
-            string acc;
-            string[] some;
-            data = result.RawResult.Split(',');
-            for (int i = 0; i < data.Length; i++)
-            {
-                Debug.Log(i + "Data is " + data[i]);
-            }
-#if UNITY_EDITOR
-            //Debug.Log("this is raw access " + result.RawResult);
-            //DebugText.text = "this is raw access " + result.RawResult;
-            //data = result.RawResult.Split(',');
-            //Debug.Log("this is access" + data[3]);
-            //DebugText.text = "this is raw access Data3 " + data[3];
-            //acc = data[3];
-            //some = acc.Split('"');
-            //DebugText.text = "this is raw access some3 " + some[3];
-            //Debug.Log("this is access " + some[3]);
-            //accesstoken = some[3];
-#elif UNITY_ANDROID
-            //Debug.Log("this is raw access "+result.RawResult);
-            //DebugText.text = "this is overall raw access "+result.RawResult;
-            //data = result.RawResult.Split(',');
-            //Debug.Log("this is access"+data[0]);
-            //DebugText.text = "this is raw access data 0 " + data[0];
-            // acc = data[0];
-            // some = acc.Split('"');
-            //Debug.Log("this is access " + some[3]);
-            //DebugText.text = "this is raw access some 3 " + some[3];
+//            string accesstoken;
+//            string[] data;
+//            string acc;
+//            string[] some;
+//            data = result.RawResult.Split(',');
+//            for (int i = 0; i < data.Length; i++)
+//            {
+//                Debug.Log(i + "Data is " + data[i]);
+//            }
+//#if UNITY_EDITOR
+//            //Debug.Log("this is raw access " + result.RawResult);
+//            //DebugText.text = "this is raw access " + result.RawResult;
+//            //data = result.RawResult.Split(',');
+//            //Debug.Log("this is access" + data[3]);
+//            //DebugText.text = "this is raw access Data3 " + data[3];
+//            //acc = data[3];
+//            //some = acc.Split('"');
+//            //DebugText.text = "this is raw access some3 " + some[3];
+//            //Debug.Log("this is access " + some[3]);
+//            //accesstoken = some[3];
+//#elif UNITY_ANDROID
+//            //Debug.Log("this is raw access "+result.RawResult);
+//            //DebugText.text = "this is overall raw access "+result.RawResult;
+//            //data = result.RawResult.Split(',');
+//            //Debug.Log("this is access"+data[0]);
+//            //DebugText.text = "this is raw access data 0 " + data[0];
+//            // acc = data[0];
+//            // some = acc.Split('"');
+//            //Debug.Log("this is access " + some[3]);
+//            //DebugText.text = "this is raw access some 3 " + some[3];
 
 
-             //accesstoken = some[3];
-#endif
-            authwithfirebase(aToken.TokenString);
-            Invoke(nameof(RetrieveFriendsList), 2f);
-        }
-        else
-        {
-            Debug.Log("User Cancelled login");
-            LoginWithAllAuth.Instance.ShowError("User Cancelled login");
-        }
-    }
+//             //accesstoken = some[3];
+//#endif
+//            authwithfirebase(aToken.TokenString);
+//            Invoke(nameof(RetrieveFriendsList), 2f);
+//        }
+//        else
+//        {
+//            Debug.Log("User Cancelled login");
+//            LoginWithAllAuth.Instance.ShowError("User Cancelled login");
+//        }
+//    }
     Firebase.Auth.FirebaseUser newuserdata;
     public void authwithfirebase(string accesstoken)
     {
@@ -238,68 +230,68 @@ public class Facebookauth : MonoBehaviour
         string nameis = newuser.DisplayName.ToString();
         //  LoginWithAllAuth.Instance.GetTokenIDAndOtherThing(3, "", newuser.Email, nameis, null);
     }
-    private void ProfilePictureCallBack(IGraphResult result)
-    {
-        if (result.Error == null && result.ResultDictionary != null)
-        {
-            Debug.Log("Download Result is " + result);
-            Debug.Log("Profile Picture Download");
+    //private void ProfilePictureCallBack(IGraphResult result)
+    //{
+    //    if (result.Error == null && result.ResultDictionary != null)
+    //    {
+    //        Debug.Log("Download Result is " + result);
+    //        Debug.Log("Profile Picture Download");
 
-            if (result.ResultDictionary.TryGetValue("picture", out object pictureObject) && pictureObject is Dictionary<string, object> pictureData)
-            {
-                Debug.Log("Hereeee");
-                if (pictureData.TryGetValue("data", out object dataObject) && dataObject is Dictionary<string, object> data)
-                {
-                    if (data.TryGetValue("url", out object urlObject) && urlObject is string url)
-                    {
-                        StartCoroutine(LoadProfilePicture(url));
-                    }
-                }
-            }
-        }
-        else
-        {
-            Debug.Log("Failed to retrieve profile picture: " + result.Error);
-        }
-    }
+    //        if (result.ResultDictionary.TryGetValue("picture", out object pictureObject) && pictureObject is Dictionary<string, object> pictureData)
+    //        {
+    //            Debug.Log("Hereeee");
+    //            if (pictureData.TryGetValue("data", out object dataObject) && dataObject is Dictionary<string, object> data)
+    //            {
+    //                if (data.TryGetValue("url", out object urlObject) && urlObject is string url)
+    //                {
+    //                    StartCoroutine(LoadProfilePicture(url));
+    //                }
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Failed to retrieve profile picture: " + result.Error);
+    //    }
+    //}
 
-    private void ProfilePictureCallBack1(IGraphResult result)
-    {
-        if (result.Error == null && !string.IsNullOrEmpty(result.RawResult))
-        {
-            string rawResult = result.RawResult;
-            string pattern = "\"url\":\"([^\"]+)\"";
-            Match match = Regex.Match(rawResult, pattern);
+    //private void ProfilePictureCallBack1(IGraphResult result)
+    //{
+    //    if (result.Error == null && !string.IsNullOrEmpty(result.RawResult))
+    //    {
+    //        string rawResult = result.RawResult;
+    //        string pattern = "\"url\":\"([^\"]+)\"";
+    //        Match match = Regex.Match(rawResult, pattern);
 
-            if (match.Success && match.Groups.Count > 1)
-            {
-                string url = match.Groups[1].Value;
+    //        if (match.Success && match.Groups.Count > 1)
+    //        {
+    //            string url = match.Groups[1].Value;
 
-                // Remove backslashes from the URL
-                url = url.Replace("\\", "");
+    //            // Remove backslashes from the URL
+    //            url = url.Replace("\\", "");
 
-                // Validate the URL format
-                if (Uri.TryCreate(url, UriKind.Absolute, out Uri profilePictureUri))
-                {
-                    StartCoroutine(LoadProfilePicture(profilePictureUri.ToString()));
-                    // Use the retrieved URL
-                    Debug.Log("Profile Picture URL: " + url);
-                }
-                else
-                {
-                    Debug.Log("Invalid profile picture URL: " + url);
-                }
-            }
-            else
-            {
-                Debug.Log("Failed to extract profile picture URL from RawResult.");
-            }
-        }
-        else
-        {
-            Debug.Log("Failed to retrieve profile picture: " + result.Error);
-        }
-    }
+    //            // Validate the URL format
+    //            if (Uri.TryCreate(url, UriKind.Absolute, out Uri profilePictureUri))
+    //            {
+    //                StartCoroutine(LoadProfilePicture(profilePictureUri.ToString()));
+    //                // Use the retrieved URL
+    //                Debug.Log("Profile Picture URL: " + url);
+    //            }
+    //            else
+    //            {
+    //                Debug.Log("Invalid profile picture URL: " + url);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("Failed to extract profile picture URL from RawResult.");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Failed to retrieve profile picture: " + result.Error);
+    //    }
+    //}
 
 
 
